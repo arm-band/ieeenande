5b6b2d56b62265b3fe3a4dc0cdab4d7433c65938
Arm Band <bre4tn.let@gmail.com>
2018-03-11 18:47:46 +0900

    全体調整・作りこみ

--- gulpfile.js
+++ gulpfile.js
@@ -112,7 +112,7 @@ gulp.task("imagemin", () => {
 
 //js圧縮&結合&リネーム
 gulp.task("js.concat", () => {
-	return gulp.src([dir.assets.jquery + "/jquery.min.js", dir.assets.barba + "/barba.min.js", dir.assets.bootstrap + "/bootstrap.min.js", dir.assets.easing + "/jquery.easing.js"])
+	return gulp.src([dir.assets.jquery + "/jquery.min.js", dir.assets.barba + "/barba.min.js", dir.assets.bootstrap + "/bootstrap.min.js", dir.assets.easing + "/jquery.easing.js", dir.src.js + "/geometryangle.min.js"])
 		.pipe(plumber())
 		.pipe(concat("lib.js"))
 		.pipe(gulp.dest(dir.src.js + "/concat/")); //srcとdistを別ディレクトリにしないと、自動でタスクが走る度にconcatしたものも雪だるま式に追加されていく
